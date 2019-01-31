0b1a6c98c6ed5b8c0405c9ba267ef8afe50ee1cc
SeverinM <severinmichaut@gmail.com>
2019-01-31 19:01:17 +0100

    First commit

---
+++ minicraft/src/engine_minicraft.h
@@ -0,0 +1,339 @@
+#ifndef __YOCTO__ENGINE_TEST__
+#define __YOCTO__ENGINE_TEST__
+
+#include "engine/engine.h"
+
+#include "avatar.h"
+#include "world.h"
+
+class MEngineMinicraft : public YEngine {
+
+public :
+	YVbo * vbo;
+	YVbo  * vbo2;
+	unsigned int prog;
+	float colors[3];
+	SYSTEMTIME time;
+	SYSTEMTIME beginDay;
+	float diff;
+	unsigned int locationUniform;
+	int hourOffset;
+	int minOffset;
+	int xMouse;
+	int yMouse;
+	bool RightHold;
+	bool CtrlHold;
+	bool wheelHold;
+
+	YColor * Day;
+	YColor * Night;
+
+	//Gestion singleton
+	static YEngine * getInstance()
+	{
+		if (Instance == NULL)
+			Instance = new MEngineMinicraft();
+		return Instance;
+	}
+
+	/*HANDLERS GENERAUX*/
+	void loadShaders() {
+		prog = Renderer->createProgram("shaders/sun");
+	}
+
+	void SetFace(YVbo * vbo, YVec3f origin, YVec3f directionFirst, YVec3f directionSecond, float size, int startingIndex, bool drawQuad = false , int nbList = 0)
+	{
+		YVec3f destination1 = (origin + (directionFirst.normalize() * size));
+		YVec3f destination2 = (origin + (directionSecond.normalize() * size));
+
+		vbo->setElementValue(nbList, startingIndex, origin.X, origin.Y, origin.Z);
+		vbo->setElementValue(nbList, startingIndex + 1, destination1.X, destination1.Y, destination1.Z);
+		vbo->setElementValue(nbList, startingIndex + 2, destination2.X, destination2.Y, destination2.Z);
+
+		if (drawQuad)
+		{
+			YVec3f destination3 = origin + (directionFirst.normalize() * size) + (directionSecond.normalize() * size);
+			vbo->setElementValue(nbList, startingIndex + 3, destination3.X, destination3.Y, destination3.Z);
+			vbo->setElementValue(nbList, startingIndex + 4, destination2.X, destination2.Y, destination2.Z);
+			vbo->setElementValue(nbList, startingIndex + 5, destination1.X, destination1.Y, destination1.Z);
+		}
+	}
+
+	void init() 
+	{
+		yMouse = -1;
+		xMouse = -1;
+		Day = new YColor(1, 1, 0.4f, 1);
+		Night = new YColor(0, 0, 0.3f, 1);
+		YLog::log(YLog::ENGINE_INFO,"Minicraft Started : initialisation");
+		GetLocalTime(&time);
+
+		glTranslatef(0.5f, -0.5f, 0);
+		glPushMatrix();
+
+		beginDay = time;
+		beginDay.wHour = 0;
+		beginDay.wMinute = 0;
+		beginDay.wSecond = 0;
+
+		Renderer->setBackgroundColor(YColor(0.0f,0.0f,0.0f,1.0f));
+		Renderer->Camera->setPosition(YVec3f(10, 10, 10));
+		Renderer->Camera->setLookAt(YVec3f());
+
+		vbo = new YVbo(3, 36, YVbo::DATA_STORAGE_METHOD::PACK_BY_VERTICE);
+
+		vbo->setElementDescription(0, YVbo::Element(3)); //Positions
+		vbo->setElementDescription(1, YVbo::Element(3)); //Normales
+		vbo->setElementDescription(2, YVbo::Element(2)); //UV
+
+		vbo->createVboCpu();
+		
+		float size(1);
+
+		SetFace(vbo, YVec3f(0, 0, 0), YVec3f(1, 0, 0), YVec3f(0, 1, 0), size, 0, true);
+		SetFace(vbo, YVec3f(0, 0, 0), YVec3f(1, 0, 0), YVec3f(0, 0, 1), size, 6, true);
+		SetFace(vbo, YVec3f(0, 0, 0), YVec3f(0, 1, 0), YVec3f(0, 0, 1), size, 12, true);
+
+		SetFace(vbo, YVec3f(0, 0, size), YVec3f(1, 0, 0), YVec3f(0, 1, 0), size, 18, true);
+		SetFace(vbo, YVec3f(size, size, size), YVec3f(0, 0, -1), YVec3f(-1, 0, 0), size, 24, true);
+		SetFace(vbo, YVec3f(size, size, size), YVec3f(0, 0, -1), YVec3f(0, -1, 0), size, 30, true);
+
+
+		for (int i = 0; i < 36; i++)
+		{
+			vbo->setElementValue(1, i, 0.5f, 0.8f, 0.5f, 1);
+		}
+
+		vbo->createVboGpu();
+		vbo->deleteVboCpu();
+
+		//vbo2
+		vbo2 = new YVbo(3, 36, YVbo::DATA_STORAGE_METHOD::PACK_BY_VERTICE);
+
+		vbo2->setElementDescription(0, YVbo::Element(3)); //Positions
+		vbo2->setElementDescription(1, YVbo::Element(3)); //Normales
+		vbo2->setElementDescription(2, YVbo::Element(2)); //UV
+
+		vbo2->createVboCpu();
+
+		SetFace(vbo2, YVec3f(0, 0, 0), YVec3f(1, 0, 0), YVec3f(0, 1, 0), size, 0, true);
+		SetFace(vbo2, YVec3f(0, 0, 0), YVec3f(1, 0, 0), YVec3f(0, 0, 1), size, 6, true);
+		SetFace(vbo2, YVec3f(0, 0, 0), YVec3f(0, 1, 0), YVec3f(0, 0, 1), size, 12, true);
+
+		SetFace(vbo2, YVec3f(0, 0, size), YVec3f(1, 0, 0), YVec3f(0, 1, 0), size, 18, true);
+		SetFace(vbo2, YVec3f(size, size, size), YVec3f(0, 0, -1), YVec3f(-1, 0, 0), size, 24, true);
+		SetFace(vbo2, YVec3f(size, size, size), YVec3f(0, 0, -1), YVec3f(0, -1, 0), size, 30, true);
+
+
+		for (int i = 0; i < 36; i++)
+		{
+			vbo2->setElementValue(1, i, 0.5f, 0.8f, 0.5f, 1);
+		}
+
+		vbo2->createVboGpu();
+		vbo2->deleteVboCpu();
+		
+		glDisable(GL_CULL_FACE);
+		glDisable(GL_DEPTH_TEST);
+	}
+
+	void update(float elapsed) 
+	{
+		GetLocalTime(&time);
+		time.wHour += hourOffset;
+		time.wHour %= 24;
+		time.wMinute += minOffset;
+		time.wMinute %= 60;
+	}
+
+	void renderObjects() 
+	{
+
+		diff = (DiffTimeMs(time, beginDay) % 86400) / 86400.0f;
+		glUseProgram(0);
+		//Rendu des axes
+		glDisable(GL_LIGHTING);
+		glBegin(GL_LINES);
+		glColor3d(1, 0, 0);
+		glVertex3d(0, 0, 0);
+		glVertex3d(10000, 0, 0);
+		glColor3d(0, 1, 0);
+		glVertex3d(0, 0, 0);
+		glVertex3d(0, 10000, 0);
+		glColor3d(0, 0, 1);
+		glVertex3d(0, 0, 0);
+		glVertex3d(0, 0, 10000);
+		glEnd();
+
+		Renderer->setBackgroundColor(Day->interpolateHSV(*Night,abs(diff - 0.5f) * 2));
+
+		//Vbo
+		glUseProgram(prog);
+		locationUniform = glGetUniformLocation(prog, "sun_color");
+		colors[2] = (time.wSecond + (time.wMilliseconds / 1000.0f));
+		colors[1] = time.wMinute + (colors[2] / 60.0f);
+		colors[0] = time.wHour + (colors[1] / 60.0f);
+
+		colors[2] /= 60.0f;
+		colors[1] /= 60.0f;
+		colors[0] /= 24.0f;
+
+		//Secondes
+		glUniform3f(locationUniform, 1, 1, abs(colors[2] - 0.5f) * 2);
+		glPushMatrix();
+		glRotatef((-360 * colors[2]) - 90, 0, 1, 0);
+		glTranslatef(5,0,0);
+
+		Renderer->updateMatricesFromOgl();
+		Renderer->sendMatricesToShader(prog);
+		vbo->render();
+
+		glPopMatrix();
+
+		//Minutes
+		glUniform3f(locationUniform, 1, abs(colors[1] - 0.5f) * 2,1);
+
+		glPushMatrix();
+		glRotatef((-360 * colors[1]) - 90, 0, 1, 0);
+		glTranslatef(3, 0, 0);
+
+		Renderer->updateMatricesFromOgl();
+		Renderer->sendMatricesToShader(prog);
+		vbo->render();
+
+		glPopMatrix();
+
+		//Heures
+		glUniform3f(locationUniform, abs(colors[0] - 0.5f) * 2, 1, 1);
+		glPushMatrix();
+		glRotatef((-360 * colors[0]) - 90, 0, 1, 0);
+		glTranslatef(1, 0, 0);
+
+		Renderer->updateMatricesFromOgl();
+		Renderer->sendMatricesToShader(prog);
+		vbo->render();
+
+		glPopMatrix();
+	}
+
+	void resize(int width, int height) {
+	
+	}
+
+	/*INPUTS*/
+	void keyPressed(int key, bool special, bool down, int p1, int p2) 
+	{
+		if (key == 'g' && down)
+		{
+			minOffset += 5;
+			while (minOffset > 60)
+			{
+				minOffset -= 60;
+				hourOffset++;
+			}
+		}
+
+		if ((key == GLUT_KEY_CTRL_L || key == GLUT_KEY_CTRL_R) && down)
+		{
+			CtrlHold = true;
+		}
+
+		if ((key == GLUT_KEY_CTRL_L || key == GLUT_KEY_CTRL_R) && !down)
+		{
+			CtrlHold = false;
+		}
+
+		if (key == 's')
+		{
+			Renderer->Camera->move(-Renderer->Camera->Direction.normalize());
+		}
+
+		if (key == 'z')
+		{
+			Renderer->Camera->move(Renderer->Camera->Direction.normalize());
+		}
+
+		if (key == 'q')
+		{
+			Renderer->Camera->move(-Renderer->Camera->RightVec.normalize());
+		}
+		
+		if (key == 'd')
+		{
+			Renderer->Camera->move(Renderer->Camera->RightVec.normalize());
+		}
+	}
+
+	void mouseWheel(int wheel, int dir, int x, int y, bool inUi)
+	{
+		if (dir > 0)
+		{
+			Renderer->Camera->IncrementFovY(1);
+		}
+		if (dir < 0)
+		{
+			Renderer->Camera->IncrementFovY(-1);
+		}
+	}
+
+	void mouseClick(int button, int state, int x, int y, bool inUi)
+	{
+		if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)
+		{
+			RightHold = true;
+		}
+
+		if (button == GLUT_MIDDLE_BUTTON && state == GLUT_DOWN)
+		{
+			wheelHold = true;
+		}
+
+		if (button == GLUT_MIDDLE_BUTTON && state == GLUT_UP)
+		{
+			wheelHold = false;
+		}
+
+		if (button == GLUT_RIGHT_BUTTON && state == GLUT_UP)
+		{
+			RightHold = false;
+		}
+	}
+
+	//x , y = position dans l'ecran
+	void mouseMove(int x, int y, bool pressed, bool inUi)
+	{
+		showMouse(pressed);
+		float deltaX(x - xMouse);
+		float deltaY(y - yMouse);
+		if (xMouse > 0 && yMouse > 0)
+		{
+			if (wheelHold)
+			{
+				Renderer->Camera->moveOnXY(-deltaX / 20.0f,-deltaY / 20.0f, CtrlHold);
+			}
+			else
+			{
+				if (RightHold)
+				{
+					if (CtrlHold)
+					{
+						Renderer->Camera->rotateAround(deltaX / 2.0f);
+						Renderer->Camera->rotateUpAround(deltaY / 5.0f);
+					}
+					else
+					{
+						Renderer->Camera->rotate(deltaX / 2.0f);
+						Renderer->Camera->rotateUp(deltaY / 5.0f);
+					}
+				}
+			}
+
+		}
+		xMouse = x;
+		yMouse = y;
+	}
+	
+};
+
+
+#endif
---
+++ src/engine/render/camera.h
@@ -0,0 +1,243 @@
+#pragma once
+
+
+class YCamera
+{
+public:
+
+	typedef enum {
+		PROJECTION_ORTHO,
+		PROJECTION_PERSPECTIVE
+	} PROJTYPE;
+
+	
+	YVec3<float> Position; ///< Position de la camera
+	YVec3<float> LookAt; ///< Point regarde par la camera
+	YVec3<float> Direction; ///< Direction de la camera
+	YVec3<float> UpVec; ///< Vecteur up de la camera
+	YVec3<float> RightVec; ///< Si on se place dans la camera, indique la droite	
+	YVec3<float> UpRef; ///< Ce qu'on considère comme le "haut" dans notre monde (et pas le up de la cam)
+	
+
+	PROJTYPE ProjType; ///< Type de projection
+	float FovY; ///< fov angle sur y in degrees
+	float Ratio; ///< with / height
+	float Left; ///< Left YPlane
+	float Right;
+	float Bottom;
+	float Top;
+	float Near;
+	float Far;
+			
+	YCamera()
+	{
+		Position = YVec3<float>(0, -1, 0);
+		LookAt = YVec3<float>(0, 0, 0);
+		UpRef = YVec3<float>(0, 0, 1);
+		UpVec = UpRef;
+		FovY = 45.0f;
+		Ratio = 800.0f / 600.0f;
+		Near = 0.1f;
+		Far = 800.0f;
+		setProjectionPerspective(FovY, Ratio, Near, Far);
+		updateVecs();
+	}
+
+	void setProjectionPerspective(float fovy, float ratio, float nearPlane, float farPlane) {
+		ProjType = PROJECTION_PERSPECTIVE;
+		FovY = fovy;
+		Ratio = ratio;
+		Near = nearPlane;
+		Far = farPlane;
+	}
+
+	void setProjectionOrtho(float left, float right, float bottom, float top, float nearPlane, float farPlane) {
+		ProjType = PROJECTION_ORTHO;
+		Left = left;
+		Right = right;
+		Bottom = bottom;
+		Top = top;
+		Near = nearPlane;
+		Far = farPlane;
+	}
+
+
+	/**
+	* Mise a jour de la camera
+	*/
+	virtual void update(float elapsed)
+	{
+
+	}
+
+	/**
+	* Definition du point regarde
+	*/
+	void setLookAt(YVec3<float> lookat)
+	{
+		LookAt = lookat;
+		updateVecs();
+	}
+
+	/**
+	* Definition de la position de la camera
+	*/
+	void setPosition(YVec3<float> pos)
+	{
+		Position = pos;
+		updateVecs();
+	}
+
+	/**
+	* Definition du haut de notre monde
+	*/
+	void setUpRef(YVec3<float> upRef)
+	{
+		UpRef = upRef;
+		updateVecs();
+	}
+
+	/**
+	* Deplacement de la camera d'un delta donné
+	*/
+	void move(YVec3<float> delta)
+	{
+		Position += delta;
+		LookAt += delta;
+		updateVecs();
+	}
+
+	void moveOnXY(float value1, float value2, bool fromWorld = false)
+	{
+		YVec3f output(0, 0, 0);
+
+		if (!fromWorld)
+		{
+			//Sur Right
+			output += RightVec * value1;
+			//Sur Up
+			output += UpVec * value2;
+		}
+
+		else
+		{
+			output += YVec3f(1, 0, 0) * value1;
+			output += YVec3f(0, 1, 0) * value2;
+		}
+		
+		LookAt += output;
+		Position += output;
+		updateVecs();
+	}
+
+	/**
+	* Deplacement de la camera à un point donné
+	*/
+	void moveTo(const YVec3<float> & target)
+	{
+		
+	}
+
+	/**
+	* On recalcule les vecteurs utiles au déplacement de la camera (Direction, RightVec, UpVec)
+	* on part du principe que sont connus :
+	* - la position de la camera
+	* - le point regarde par la camera
+	* - la vecteur up de notre monde
+	*/
+	void updateVecs(void)
+	{
+		Direction = (LookAt - Position).normalize();
+		RightVec = Direction.cross(UpRef).normalize();
+		UpVec = RightVec.cross(Direction).normalize();
+		look();
+	}
+
+	/**
+	* Rotation droite gauche en subjectif
+	*/
+	//Rotation en Z et en degrés : convertit en radians
+	void rotate(float angle)
+	{
+		angle *= (M_PI / 180.0f);
+		LookAt -= Position;
+		LookAt.rotate(UpVec, angle);
+		LookAt += Position;
+	}
+
+	/**
+	* Rotation haut bas en subjectif
+	*/
+	void rotateUp(float angle)
+	{
+		angle *= (M_PI / 180.0f);
+		LookAt -= Position;
+		LookAt.rotate(RightVec, angle);
+		LookAt += Position;
+	}
+
+	/**
+	* Rotation droite gauche en troisième personne
+	*/
+	void rotateAround(float angle)
+	{
+		angle *= (M_PI / 180.0f);
+		YVec3f delta(LookAt - Position);
+		YVec3f lookAtOrigin(LookAt);
+		LookAt = YVec3f(0, 0, 0);
+		Position = -delta;
+		Position.rotate(UpVec, angle);
+		delta = LookAt - Position;
+		LookAt = lookAtOrigin;
+		Position = LookAt - delta;
+		updateVecs();
+	}
+
+	/**
+	* Rotation haut bas en troisième personne
+	*/
+	void rotateUpAround(float angle)
+	{
+		angle *= (M_PI / 180.0f);
+		YVec3f delta(LookAt - Position);
+		YVec3f lookAtOrigin(LookAt);
+		LookAt = YVec3f(0, 0, 0);
+		Position = -delta;
+		Position.rotate(RightVec, angle);
+		delta = LookAt - Position;
+		LookAt = lookAtOrigin;
+		Position = LookAt - delta;
+		updateVecs();
+	}
+
+	void SetFovY(float value)
+	{
+		value = abs(value);
+		FovY = max(1, min(89, value));
+	}
+
+	void IncrementFovY(float delta)
+	{
+		SetFovY(FovY + delta);
+	}
+
+	/**
+	* Calcul du bon repère de départ pour la matrice monde
+	*/
+	void look(void)
+	{
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		if (ProjType == PROJECTION_PERSPECTIVE) {
+			gluPerspective(FovY, Ratio, Near, Far);
+		}
+		if (ProjType == PROJECTION_ORTHO) {
+			glOrtho(Left, Right, Bottom, Top, Near, Far);
+		}
+		
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		gluLookAt(Position.X, Position.Y, Position.Z, LookAt.X, LookAt.Y, LookAt.Z, UpVec.X, UpVec.Y, UpVec.Z);
+		
+	}
+};
