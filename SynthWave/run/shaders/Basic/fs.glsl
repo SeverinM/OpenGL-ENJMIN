fd6549201f54cb6887932958f66eccd24e880601
SeverinM <severinmichaut@gmail.com>
2019-03-09 18:08:19 +0100

    fgd

--- SynthWave/run/shaders/Basic/fs.glsl
+++ SynthWave/run/shaders/Basic/fs.glsl
@@ -2,25 +2,31 @@
 
 in vec4 worldPos_vs;
 in vec4 normal_vs;
+in vec4 view_vs;
 
 layout (location = 0) out vec4 color;
 layout (location = 1) out vec4 passColor;
 
-vec4 ambient = vec4(0, 0, 1, 1);
+vec4 lightColor = vec4(1, 1, 204.0 / 255.0, 1);
+float ambiantStrength = 0.1;
+float specularStrength = 0;
 
 uniform vec3 sunPos;
 
 void main()
 {
-	vec4 interColor;
-	interColor = vec4(normalize(sunPos - worldPos_vs.xyz),1);
-
+	vec4 reflect = reflect(-view_vs, normal_vs);
+	float spec = pow(max(dot(view_vs.xyz, reflect.xyz), 0.0), 256);
 	vec3 toSun = normalize(sunPos - worldPos_vs.xyz);
-	interColor = vec4(vec3(1, 1, 1) * max(0,dot(toSun, normal_vs.xyz)),1);
 
-	interColor = normalize(interColor + ambient);
+	vec4 interColor;
+	vec3 ambiant = ambiantStrength * lightColor.rgb;
+	vec3 diffuse = lightColor.rgb * max(0, dot(toSun, normal_vs.xyz));
+	vec3 specular = lightColor.rgb * spec * specularStrength;
+
+	interColor = vec4(vec3(0.1, 0.1, 0.1) * (specular + diffuse + ambiant), 1);
 	interColor.a = 1;
 
 	color = interColor;
-	passColor = color;
+	passColor = vec4(0, 0, 0, 1);
 }
--- SynthWave/run/shaders/Basic/vs.glsl
+++ SynthWave/run/shaders/Basic/vs.glsl
@@ -3,16 +3,19 @@
 uniform mat4 mvp;
 uniform mat4 m;
 uniform mat4 nmat;
+uniform mat4 mv;
 
 layout(location = 0) in vec3 position_in;
 layout(location = 1) in vec3 normal_in;
 
 out vec4 worldPos_vs;
 out vec4 normal_vs;
+out vec4 view_vs;
 
 void main()
 {
 	gl_Position = mvp * vec4(position_in, 1);
 	worldPos_vs = m * vec4(position_in,1);
-	normal_vs = vec4(normal_in, 1);
+	view_vs = mv * vec4(position_in, 1);
+	normal_vs = nmat * vec4(normal_in, 1);
 }
